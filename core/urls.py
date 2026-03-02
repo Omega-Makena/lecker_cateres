6c5a49bbf9745a82731920a1a5dcfdb758f7fb12
Omega Mwebia <omegam@innova.co.ke>
2026-03-02 10:38:52 +0300

    Configure project for Netlify static deployment with django-distill

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django_distill import distill_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('catering/', views.catering, name='catering'),
-    path('decoration/', views.decoration, name='decoration'),
-    path('juice-hubs/', views.juice_hubs, name='juice_hubs'),
+    distill_path('', views.home, name='home', distill_file='index.html'),
+    distill_path('catering/', views.catering, name='catering', distill_file='catering/index.html'),
+    distill_path('decoration/', views.decoration, name='decoration', distill_file='decoration/index.html'),
+    distill_path('juice-hubs/', views.juice_hubs, name='juice_hubs', distill_file='juice-hubs/index.html'),
 ]
--- lecker_project/settings.py
+++ lecker_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_distill',
     'core',
 ]
 
@@ -121,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'core' / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
